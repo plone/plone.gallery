0c5a48aedf1f5dd9af4bbaabfb20d0d027261342
MrTango <md@derico.de>
2020-06-11 20:40:32 +0300

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='plone.gallery',
-    version='1.0a2',
+    version='1.0a3.dev0',
     description="Photo gallery for Plone CMS.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
