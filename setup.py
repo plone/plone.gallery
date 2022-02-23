d32d59ed11789bbca0dcb74383df2279c4771128
MrTango <md@derico.de>
2022-02-23 15:51:32 +0200

    Fix python_requires definition

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7,>=3.7",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
