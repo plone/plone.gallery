683db56052121b2fb933446c7a05bd7f4d180b30
MrTango <md@derico.de>
2022-07-25 18:28:48 +0300

    "Add upgrade_step: Ugrade plone.gallery to load new control panel setting."

---
+++ src/plone/gallery/upgrades/v1001.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from . import logger
+
+
+from .base import reload_gs_profile
+# from plone import api
+
+
+def upgrade(setup_tool=None):
+    """
+    """
+    logger.info("Running upgrade (Python): Ugrade plone.gallery to load new control panel setting.")
+    reload_gs_profile(setup_tool)
