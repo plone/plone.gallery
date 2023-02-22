64ff6027a8a77639dd6d30ce6bf86e3b468613fd
MrTango <md@derico.de>
2023-02-22 20:38:45 +0200

    "Add upgrade_step: Load profile to enable TinyMCE templates and resources"

---
+++ src/plone/gallery/tests/test_upgrade_step_1002.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+# from plone.gallery.testing import PLONE_GALLERY_FUNCTIONAL_TESTING
+from plone.gallery.testing import PLONE_GALLERY_INTEGRATION_TESTING
+
+import unittest
+
+
+class UpgradeStepIntegrationTest(unittest.TestCase):
+
+    layer = PLONE_GALLERY_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_upgrade_step(self):
+        # dummy, add tests here
+        self.assertTrue(True)
+
+
+# class UpgradeStepFunctionalTest(unittest.TestCase):
+#
+#     layer = PLONE_GALLERY_FUNCTIONAL_TESTING
+#
+#     def setUp(self):
+#         self.portal = self.layer['portal']
+#         setRoles(self.portal, TEST_USER_ID, ['Manager'])
---
+++ src/plone/gallery/upgrades/v1002.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from . import logger
+
+
+from .base import reload_gs_profile
+# from plone import api
+
+
+def upgrade(setup_tool=None):
+    """
+    """
+    logger.info("Running upgrade (Python): Load profile to enable TinyMCE templates and resources")
+    reload_gs_profile(setup_tool)
