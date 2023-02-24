d9cb776b7f65b369bdcd67f6e1cb094693f2a528
MrTango <md@derico.de>
2023-02-24 14:55:45 +0200

    fix setup py

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
-        "Framework :: Plone :: Classic UI",
         "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
         "Programming Language :: Python",
